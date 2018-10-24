9c22458c739d7c3b053f558db1d1c09b31fc2f59
RuiCoimbras <rui.coimbras@pt.ibm.com>
2018-10-24 12:41:09 +0100

    Add

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,6 +1,19 @@
 // src/services/product-service.js
+var axios = require('axios')
+
 module.exports = {
-    getProductWeight: async function(productId) {
-      // Meanwhile it can be implemented by other developers
-    }
-};
+  getProductWeight: async function (productId) {
+    return axios
+      .get('https://mycluster.icp:8899/products/' + productId)
+      .then(response => {
+        if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
+          return response.data.weightLB
+        } else {
+          return Promise.reject('Invalid response object')
+        }
+      })
+      .catch( (err) => {
+        throw new Error(err)
+      })
+  }
+}
